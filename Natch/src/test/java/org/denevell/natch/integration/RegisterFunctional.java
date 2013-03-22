d063f5a74f34b4bddb853be2b4418c2d1b0f2ade
denevell <aaron@denevell.org>
2013-03-22 00:14:14 +0000

    Register functional tests now working

--- Natch/src/test/java/org/denevell/natch/integration/RegisterFunctional.java
+++ Natch/src/test/java/org/denevell/natch/integration/RegisterFunctional.java
@@ -3,6 +3,10 @@ package org.denevell.natch.integration;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import javax.ws.rs.core.MediaType;
 
 import org.denevell.natch.register.RegisterResourceInput;
@@ -15,15 +19,21 @@ import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 
-public class Register {
+public class RegisterFunctional {
 	
 	private WebResource service;
 
 	@Before
-	public void setup() {
+	public void setup() throws IOException, InterruptedException {
 	    ClientConfig config = new DefaultClientConfig();
 	    Client client = Client.create(config);
 	    service = client.resource(IntegrationTestUtils.getBaseURI());
+	    
+	    Process p = Runtime.getRuntime().exec("rm -f /var/lib/tomcat7/dbs/test.db");
+	    p.waitFor();
+	    BufferedReader result = new BufferedReader(new  InputStreamReader(p.getErrorStream()));
+	    System.out.println(result.readLine());
+	    
 	}
 
 	@Test
@@ -57,9 +67,15 @@ public class Register {
 	    		.path("user")
 	    		.type(MediaType.APPLICATION_JSON)
 	    		.put(RegisterResourceReturnData.class, registerInput);
+		RegisterResourceReturnData result2 = service
+	    		.path("rest")
+	    		.path("user")
+	    		.type(MediaType.APPLICATION_JSON)
+	    		.put(RegisterResourceReturnData.class, registerInput);
 		
 		// Assert
-		assertFalse("Should return false as 'successful' field", result.isSuccessful());
+		assertTrue("Should return true as 'successful' field", result.isSuccessful());
+		assertFalse("Should return false as 'successful' field", result2.isSuccessful());
 	}
 	
 	public void register_shouldSeeErrorJsonOnBadJsonPassed() {
