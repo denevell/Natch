97de41023622fb1344a0314897d170a7ae77d0ec
denevell <aaron@denevell.org>
2013-04-25 18:29:07 +0100

    Moved over to hibernate for JPA due to potential leaks with eclipselink.
    Also changed the delete all db entries method to do it the JPA way to avoid foreign key db errors.

--- src/main/java/org/denevell/natch/serv/testutils/RegisterModel.java
+++ src/main/java/org/denevell/natch/serv/testutils/RegisterModel.java
@@ -1,12 +1,16 @@
 package org.denevell.natch.serv.testutils;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 
 import org.denevell.natch.db.entities.PersistenceInfo;
+import org.denevell.natch.db.entities.PostEntity;
+import org.denevell.natch.db.entities.ThreadEntity;
+import org.denevell.natch.db.entities.UserEntity;
 import org.denevell.natch.utils.EntityUtils;
 
 public class RegisterModel {
@@ -22,12 +26,18 @@ public class RegisterModel {
 	public void clearTestDb() {
 		EntityTransaction trans = mEntityManager.getTransaction();
 		trans.begin();
-		Query q = mEntityManager.createQuery("delete from ThreadEntity");
-		q.executeUpdate();
-		q = mEntityManager.createQuery("delete from PostEntity");
-		q.executeUpdate();
-		q = mEntityManager.createQuery("delete from UserEntity");
-		q.executeUpdate();
+		List<PostEntity> result = mEntityManager.createQuery("select a from PostEntity a", PostEntity.class).getResultList();
+		for (PostEntity postEntity : result) {
+			mEntityManager.remove(postEntity);
+		}
+		List<ThreadEntity> resultT = mEntityManager.createQuery("select a from ThreadEntity a", ThreadEntity.class).getResultList();
+		for (ThreadEntity postEntity : resultT) {
+			mEntityManager.remove(postEntity);
+		}
+		List<UserEntity> resultU = mEntityManager.createQuery("select a from UserEntity a", UserEntity.class).getResultList();
+		for (UserEntity postEntity : resultU) {
+			mEntityManager.remove(postEntity);
+		}
 		trans.commit();
 		EntityUtils.closeEntityConnection(mFactory, mEntityManager);
 	}
