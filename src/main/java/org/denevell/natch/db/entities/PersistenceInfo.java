1cdfddeacd338cb647999e7e6192c015d700fa1a
denevell <aaron@denevell.org>
2013-05-20 01:08:26 +0100

    Moved two persistence.xml files into one.
    I'll implement compile time switches via environmental variables next.

--- src/main/java/org/denevell/natch/db/entities/PersistenceInfo.java
+++ src/main/java/org/denevell/natch/db/entities/PersistenceInfo.java
@@ -2,6 +2,6 @@ package org.denevell.natch.db.entities;
 
 public class PersistenceInfo {
 
-	public static final String EntityManagerFactoryName = "users";
+	public static final String EntityManagerFactoryName = "test";
 
 }
