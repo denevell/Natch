d16b7e3e89ca4116f9826a4747f652bce5b4aa22
denevell <aaron@denevell.org>
2013-08-02 01:24:26 +0100

    Moved most thread entity mappings to xml.
    Test db is dropped on init now.

--- src/main/java/org/denevell/natch/db/entities/ThreadEntity.java
+++ src/main/java/org/denevell/natch/db/entities/ThreadEntity.java
@@ -3,37 +3,13 @@ package org.denevell.natch.db.entities;
 import java.util.List;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
-@NamedQueries(
-	{
-	@NamedQuery(name=ThreadEntity.NAMED_QUERY_LIST_THREADS,query=
-		"select p from ThreadEntity p where p.rootPost is not null order by p.latestPost.created desc")
-	,
-	@NamedQuery(name=ThreadEntity.NAMED_QUERY_LIST_THREADS_BY_TAG,query=
-		"select p from ThreadEntity p where :" + ThreadEntity.NAMED_QUERY_PARAM_TAG +  " member of p.rootPost.tags order by p.latestPost.created desc")
-	,
-	@NamedQuery(name=ThreadEntity.NAMED_QUERY_FIND_THREAD_BY_ID,query=
-		"select p from ThreadEntity p where p.id = :" + ThreadEntity.NAMED_QUERY_PARAM_ID)
-	,
-	@NamedQuery(name=ThreadEntity.NAMED_QUERY_FIND_AUTHOR,query=
-		"select p.rootPost.user from ThreadEntity p where p.id = :" + ThreadEntity.NAMED_QUERY_PARAM_ID)
-	}
-)
-@Entity
 public class ThreadEntity {
-	@Id 
 	private String id;
-	@OneToOne
 	private PostEntity latestPost;
 	@OneToMany(cascade=CascadeType.PERSIST) 
 	private List<PostEntity> posts;
-	@OneToOne
 	private PostEntity rootPost;
 	public static final String NAMED_QUERY_LIST_THREADS = "findThreads";
 	public static final String NAMED_QUERY_LIST_THREADS_BY_TAG = "findThreadByTag";
