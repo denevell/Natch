b7acb5872d5a3899ef11ad33c4390247795d4b11
denevell <aaron@denevell.org>
2013-11-13 00:34:04 +0000

    More work on the migration script.

--- db_migration.go
+++ db_migration.go
@@ -5,46 +5,113 @@ import (
 	"database/sql"
 	_ "github.com/lib/pq"
 	"reflect"
+	"os"
 	"fmt"
 	"log"
 )
 
-type migration string
+var con *sql.DB
+
+type migration struct {
+	Tx *sql.Tx
+}
+
+func versionsAvailable() int {
+	for i := 1; i <= 100; i++ {
+		var m migration
+		v := reflect.ValueOf(&m).MethodByName("Migration_"+strconv.Itoa(i));
+		if v.IsValid()==false {
+			i--
+			return i
+		}
+	}
+	return 0
+}
+
+func dbVersionNumber(con *sql.Tx) (int, error) {
+	row := con.QueryRow("SELECT version_number FROM version_number")
+	var id int 
+	err := row.Scan(&id)
+	return id, err;
+}
+
+func updateVersionNumber(con *sql.Tx, version int) error {
+	_, err := con.Exec("delete from version_number")
+	if err != nil {
+		return err
+	}
+	_, err = con.Exec("insert into version_number (version_number) values("+strconv.Itoa(version)+")")
+	return err;
+}
 
 func main() {
 	// Open db
-	con, err := sql.Open("postgres", "postgres://denevell:user@localhost:5432/testnatch")
+	var err error
+	con, err = sql.Open("postgres", "postgres://denevell:user@localhost:5432/testnatch")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer con.Close()
+	tx, err := con.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Get version number
-	versionNumber, err := dbVersionNumber(con)
+	versionNumber, err := dbVersionNumber(tx)
 	if err != nil {
 		log.Fatalf("Couldn't get version of db: %v", err)
 	}
-
 	fmt.Println("Current db version number:", versionNumber)
 
+	// Get number of versions in script
+	versionsAvailable := versionsAvailable() 
+	fmt.Printf("Versions available: 1-%d\n", versionsAvailable)
+	fmt.Println("---");
+
 	// Get migration functions
-	for i := versionNumber; i <= 2; i++ {
-		var m migration
+	for i := versionNumber; i <= versionsAvailable; i++ {
+		defer func() {
+			if e := recover(); e != nil {
+				fmt.Println("Rolling back")
+				err := tx.Rollback()
+				if err != nil {
+					fmt.Println("Rollback error: ", err)
+				}
+				fmt.Println("Failed migration:", e)
+				os.Exit(1)
+			}
+		}()
+		m := migration{tx}
+		fmt.Printf("### Running Migration_%d\n", i)
 		reflect.ValueOf(&m).MethodByName("Migration_"+strconv.Itoa(i)).Call([]reflect.Value{})
+		err := updateVersionNumber(tx, i+1)
+		if err != nil {
+			panic(err)
+		}
 	}
+
+	tx.Commit()
 }
 
 func (m migration) Migration_1() {
-	fmt.Println("One")
+	_, err := m.Tx.Exec("create table userentity (username varchar(50) not null primary key, admin bool, password varchar(200) not null)");
+	if err != nil {
+		panic(err)
+	}
 }
 
 func (m migration) Migration_2() {
-	fmt.Println("Two")
-}
-
-func dbVersionNumber(con *sql.DB) (int, error) {
-	row := con.QueryRow("SELECT version_number FROM info")
-	var id int 
-	err := row.Scan(&id)
-	return id, err;
+	_, err := m.Tx.Exec("create table postentity (id integer not null primary key, content text, subject varchar(300), threadId integer, created integer, modified integer)");
+	if err != nil {
+		panic(err)
+	}
+	_, err = m.Tx.Exec("create table postentity_userentity (postentity_id integer, userentity integer");
+	if err != nil {
+		panic(err)
+	}
+	_, err = m.Tx.Exec("create table postentity_tags (postentity_id integer, tags varchar(20)");
+	if err != nil {
+		panic(err)
+	}
 }
