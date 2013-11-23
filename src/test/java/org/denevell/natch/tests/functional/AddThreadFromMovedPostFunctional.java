33069036d0add81acc8aa4aadaa33fc41bb4f157
denevell <aaron@denevell.org>
2013-11-23 21:24:09 +0000

    Added functional tests for move post to thread.

--- src/test/java/org/denevell/natch/tests/functional/AddThreadFromMovedPostFunctional.java
+++ src/test/java/org/denevell/natch/tests/functional/AddThreadFromMovedPostFunctional.java
@@ -2,12 +2,8 @@ package org.denevell.natch.tests.functional;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import javax.ws.rs.core.MediaType;
@@ -19,7 +15,6 @@ import org.denevell.natch.io.threads.AddThreadFromPostResourceInput;
 import org.denevell.natch.io.users.LoginResourceInput;
 import org.denevell.natch.io.users.LoginResourceReturnData;
 import org.denevell.natch.io.users.RegisterResourceInput;
-import org.denevell.natch.io.users.RegisterResourceReturnData;
 import org.denevell.natch.utils.Strings;
 import org.denevell.natch.utils.TestUtils;
 import org.junit.Before;
@@ -59,6 +54,7 @@ public class AddThreadFromMovedPostFunctional {
 		AddPostFunctional.addPost(service, loginResult.getAuthKey(), postInput);
 		// Arrange -- get posts
 		ListPostsResource posts = ListPostsFunctional.listRecentPostsThreads(service);
+		assertTrue("Should have two posts, thread starter and first post", posts.getPosts().size()==2);
 		
         // Act
 		AddThreadFromPostResourceInput threadFromPostInput = new AddThreadFromPostResourceInput();
@@ -69,10 +65,45 @@ public class AddThreadFromMovedPostFunctional {
 		AddPostResourceReturnData returnData = addThreadFromPost(service, adminLoginResult.getAuthKey(), threadFromPostInput); 
 		
 		// Assert
-		assertEquals("", returnData.getError());
-		assertNotNull(returnData.getThread().getSubject());
 		assertTrue(returnData.isSuccessful());
+		posts = ListPostsFunctional.listRecentPostsThreads(service);
+		assertEquals("New thread has right subject", posts.getPosts().get(0).getSubject(), "New subject");
+		assertEquals("New thread has right content", posts.getPosts().get(0).getContent(), "b");
+		assertTrue("Still just have two posts, since one's been moved", posts.getPosts().size()==2);
 	}
+
+	@Test
+	public void shouldThrow401WhenNotAdmin() {
+	    // Arrange -- login as other user
+	    RegisterResourceInput registerInput = new RegisterResourceInput("other", "other");
+	    RegisterFunctional.register(service, registerInput);
+		LoginResourceInput loginInput = new LoginResourceInput("other", "other");
+		LoginResourceReturnData loginResult = LoginFunctional.login(service, loginInput);
+		// Arrange -- add thread and post 
+		AddPostResourceInput threadInput = new AddPostResourceInput("c", "s");
+		AddPostResourceReturnData threadRet = AddThreadFunctional.addThread(service, loginResult.getAuthKey(), threadInput);
+		AddPostResourceInput postInput = new AddPostResourceInput("-", "b");
+		postInput.setThreadId(threadRet.getThread().getId());
+		AddPostFunctional.addPost(service, loginResult.getAuthKey(), postInput);
+		// Arrange -- get posts
+		ListPostsResource posts = ListPostsFunctional.listRecentPostsThreads(service);
+		
+        // Act
+		AddThreadFromPostResourceInput threadFromPostInput = new AddThreadFromPostResourceInput();
+		threadFromPostInput.setContent(postInput.getContent());
+		threadFromPostInput.setSubject("New subject");
+		threadFromPostInput.setPostId(posts.getPosts().get(0).getId());
+		threadFromPostInput.setUserId("other");
+		try {
+		    addThreadFromPost(service, loginResult.getAuthKey(), threadFromPostInput); 
+        } catch (UniformInterfaceException e) {
+            assertTrue(e.getResponse().getStatus()==401);
+            return;
+        }
+		assertFalse("Was exception 401 exception", true);
+	}
+	
+	// TODO: 401 when not an admin
 	
     public static AddPostResourceReturnData addThreadFromPost(WebResource service, Object authKey, AddThreadFromPostResourceInput input) {
         AddPostResourceReturnData returnData = 
